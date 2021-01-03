c86adbcc7aa415e7876100929bb6f848c9a53151
ChristoLuksatrio <christo.luksatrio@gmail.com>
2021-01-03 11:09:54 -0500

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Postable</title>
+</head>
+<body class='bg-gray-100'>
+    <nav class='p-6 bg-white flex justify-between'>
+        <ul class='flex items-center'>
+            <li>
+                <a href="" class='p-3'>Home</a>
+            </li>
+            <li>
+                <a href="" class='p-3'>Dashboard</a>
+            </li>
+            <li>
+                <a href="" class='p-3'>Posts</a>
+            </li>
+        </ul>
+        <ul class='flex items-center'>
+            <li>
+                <a href="" class='p-3'>Christo Luksatrio</a>
+            </li>
+            <li>
+                <a href="" class='p-3'>Login</a>
+            </li>
+            <li>
+                <a href="" class='p-3'>Register</a>
+            </li>
+            <li>
+                <a href="" class='p-3'>Logout</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
